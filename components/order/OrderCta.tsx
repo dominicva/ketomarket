cbffc1e65c04291974bc90ba0cbc1426fdefdb1a
Dominic van Almsick <dominicvana@gmail.com>
2023-06-15 14:29:57 +0100

    feat: add loading states to OrderCta

--- components/order/OrderCta.tsx
+++ components/order/OrderCta.tsx
@@ -1,27 +1,67 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import { TwoSeventyRing } from 'react-svg-spinners';
 import { Button } from '@/components/buttons';
 
 export function OrderCta({ cart }: { cart: any }) {
+  const [isLoadingPayment, setIsLoadingPayment] = useState(false);
+  const [isLoadingReturnToCart, setIsLoadingReturnToCart] = useState(false);
   const emptyCart = !cart?.cartItems.length;
+
+  const handlePaymentClick = () => {
+    setIsLoadingPayment(true);
+  };
+
+  const handlePaymentNavigationComplete = () => {
+    setIsLoadingPayment(false);
+  };
+
+  const handleReturnToCartClick = () => {
+    setIsLoadingReturnToCart(true);
+  };
+
+  const handleReturnToCartNavigationComplete = () => {
+    setIsLoadingReturnToCart(false);
+  };
 
   return (
     <>
-      <form action="/api/create-payment-intent" method="POST">
+      <form
+        action="/api/create-payment-intent"
+        method="POST"
+        onClick={handlePaymentClick}
+        onLoad={handlePaymentNavigationComplete}
+      >
         <Button
-          className="m-auto mt-8 block w-11/12"
+          className="m-auto mt-8 flex w-11/12 items-center justify-center"
           intent={emptyCart ? 'disabled' : 'primary'}
           size="large"
         >
-          Proceed to payment
+          {isLoadingPayment ? (
+            <TwoSeventyRing color="white" />
+          ) : (
+            <span>Proceed to payment</span>
+          )}
         </Button>
       </form>
-      <Link href={`/${emptyCart ? 'home' : 'profile/cart'}`}>
+      <Link
+        href={`/${emptyCart ? 'home' : 'profile/cart'}`}
+        passHref
+        onClick={handleReturnToCartClick}
+        onLoad={handleReturnToCartNavigationComplete}
+      >
         <Button
-          className="m-auto mt-8 block w-11/12"
+          className="m-auto mt-8 flex w-11/12 items-center justify-center"
           intent="tertiary"
           size="large"
         >
-          {emptyCart ? 'Back to store' : 'Return to Cart'}
+          {isLoadingReturnToCart ? (
+            <TwoSeventyRing color="#09624B" />
+          ) : (
+            <span>{emptyCart ? 'Back to store' : 'Return to Cart'}</span>
+          )}
         </Button>
       </Link>
     </>
