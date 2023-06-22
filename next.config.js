7133f664066ad368869b62f95624f7bb4039f371
Dominic van Almsick <dominicvana@gmail.com>
2023-06-22 15:40:18 +0100

    chore: cleanup next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const { hostname } = require('os');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     remotePatterns: [
       {
