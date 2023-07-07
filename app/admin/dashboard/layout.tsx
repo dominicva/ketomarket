be86540fc3733e37ec8d966e62c5afc173e0c703
Dominic van Almsick <dominicvana@gmail.com>
2023-07-07 10:44:28 +0100

    fix: add additional user role check

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -1,10 +1,18 @@
+import { getUserFromDb } from '@/lib/user';
 import AdminNav from '@/components/admin/AdminNav';
+import NotAuthorized from '@/components/auth/NotAuthorized';
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await getUserFromDb();
+
+  if (!user?.isAdmin) {
+    return <NotAuthorized />;
+  }
+
   return (
     <main>
       <AdminNav />
